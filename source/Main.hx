120f642171c4e86b8f81f05b8dffb11cf5ddea09
Luke Hayes <lukepurplehayes@gmail.com>
2022-05-28 13:45:13 +0100

    Removed default splash screen from Main.hx.

--- source/Main.hx
+++ source/Main.hx
@@ -5,9 +5,9 @@ import openfl.display.Sprite;
 
 class Main extends Sprite
 {
-	public function new()
-	{
-		super();
-		addChild(new FlxGame(0, 0, PlayState));
-	}
+    public function new()
+    {
+        super();
+        addChild(new FlxGame(0, 0, PlayState, 1, 60, 60, true));
+    }
 }
