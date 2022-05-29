f739d232072d15313777fe6632eafdd8d4c4e3a5
Luke Hayes <lukepurplehayes@gmail.com>
2022-05-29 10:47:39 +0100

    Added randomizeColor member and function to set the base color of the block.

--- source/block/Block.hx
+++ source/block/Block.hx
@@ -7,8 +7,22 @@ import entity.Entity;
   **/
 class Block extends Entity
 {
-    public function new(x,y, w:Int = 50, h:Int = 20)
+    public function new(x,y, w:Int = 50, h:Int = 20, randomizeColor:Bool = true)
     {
         super(x,y,w,h);
+
+        if(randomizeColor)
+            this.randomizeColor();
+    }
+
+    /**
+      Randomize the base color of the block.
+      **/
+    private function randomizeColor()
+    {
+        var r = Math.floor(Math.random() * 254);
+        var g = Math.floor(Math.random() * 254);
+        var b = Math.floor(Math.random() * 254);
+        this.color = flixel.util.FlxColor.fromRGB(r,g,b,255);
     }
 }
