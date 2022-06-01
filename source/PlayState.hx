7043db705b29fdce52db3807948db80da64c0e05
Luke Hayes <lukepurplehayes@gmail.com>
2022-06-01 10:33:41 +0100

    Added instance of Paddle to PlayState along with basic collision detection for ball/paddle.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -6,10 +6,12 @@ import flixel.FlxG;
 import block.BlockGen;
 import block.Block;
 import entity.Ball;
+import entity.Paddle;
 
 class PlayState extends FlxState
 {
     var ball : Ball;
+    var paddle : Paddle;
     var ballGrp : FlxTypedGroup<Block>;
 
     override public function create()
@@ -22,6 +24,9 @@ class PlayState extends FlxState
 
         ball = new Ball(200,300);
         add(ball);
+
+        paddle = new Paddle(FlxG.width / 2, 450, 100,10);
+        add(paddle);
     }
 
     override public function update(elapsed:Float)
@@ -29,11 +34,18 @@ class PlayState extends FlxState
         super.update(elapsed);
 
         FlxG.collide(ball, ballGrp, processCollisions);
+        FlxG.collide(ball, paddle,  processPaddleCollisions);
     }
 
     private function processCollisions(ball, block)
     {
         block.kill();
+        ball.dx = -ball.dx;
+        ball.dy = -ball.dy;
+    }
+
+    private function processPaddleCollisions(ball, paddle)
+    {
         ball.dx = -ball.dx;
         ball.dy = -ball.dy;
     }
